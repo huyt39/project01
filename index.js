ef5e0e27b60514501e1d4159c628170731c03f0b
Huy Tuan <hotuanhuy1203@gmail.com>
2024-03-23 12:44:34 +0700

    env

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express"); //nhung express
+const dotenv = require("dotenv"); //import dotenv tu thu vien dotenv
 const routesClient=require("./routes/client/index.route.js"); //loi ham route client vao
+
+dotenv.config(); //de cau hinh
+
 const app = express(); //khoi tao app dung express
-const port = 3000; //khoi tao cong
+
+const port = process.env.PORT;
+//  const port = 3000; //khoi tao cong
 
 app.set("views", "./views"); //giao dien
 app.set("view engine", "pug");
