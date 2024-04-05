ef951c84b293499d9ea6aa96afcb071f3feb4a98
Huy Tuan <hotuanhuy1203@gmail.com>
2024-04-05 13:08:08 +0700

    lay data sp in ra giao dien

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,14 @@
+const Product = require("../../models/product.model");
+
+
 // [GET] /admin/products/
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    });
+    console.log(products);
     res.render("admin/pages/products/index", {
-      pageTitle: "Danh sách sản phẩm"
+      pageTitle: "Danh sách sản phẩm",
+      products: products
     });
   }
