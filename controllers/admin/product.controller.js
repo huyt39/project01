185d7370f5d695b555c9555fa466ccd7b125e330
Huy Tuan <hotuanhuy1203@gmail.com>
2024-04-05 13:19:12 +0700

    loc theo trang thai

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -3,12 +3,48 @@ const Product = require("../../models/product.model");
 
 // [GET] /admin/products/
 module.exports.index = async (req, res) => {
-    const products = await Product.find({
-        deleted: false
-    });
-    console.log(products);
+    // const products = await Product.find({
+    //     deleted: false
+    // });
+    const filterState = [
+        {
+          name: "Tất cả",
+          status: "",
+          class: ""
+        },
+        {
+          name: "Hoạt động",
+          status: "active",
+          class: ""
+        },
+        {
+          name: "Dừng hoạt động",
+          status: "inactive",
+          class: ""
+        }
+      ];
+    
+      if(req.query.status) {
+        const index = filterState.findIndex(item => item.status == req.query.status);
+        filterState[index].class = "active";
+      } else {
+        filterState[0].class = "active";
+      }
+    
+      console.log(filterState);
+    
+      const find = {
+        deleted: false,
+      }
+    
+      if(req.query.status) {
+        find.status = req.query.status;
+      }
+    // console.log(products);
+    const products = await Product.find(find);
     res.render("admin/pages/products/index", {
       pageTitle: "Danh sách sản phẩm",
-      products: products
+      products: products,
+      filterState: filterState
     });
   }
---
+++ public/admin/js/script.js
@@ -0,0 +1,20 @@
+// Button Status
+const buttonsStatus = document.querySelectorAll("[button-status]");
+if(buttonsStatus.length > 0) {
+  let url = new URL(window.location.href);
+
+  buttonsStatus.forEach((button) => {
+    button.addEventListener("click", () => {
+      const status = button.getAttribute("button-status");
+      console.log(status);
+      if(status) {
+        url.searchParams.set("status", status);
+      } else {
+        url.searchParams.delete("status");
+      }
+
+      window.location.href = url.href;
+    });
+  });
+}
+// End Button Status
